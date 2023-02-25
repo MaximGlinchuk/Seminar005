9ed6298a8970231fe5f8e316ad27fb9d361317ae
Максим Глинчук <glinchuk.maxim@yandex.ru>
2023-02-25 21:52:11 +0400

    task38

---
+++ task38/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 38: Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+// Используйте NextDouble().
+double[] numbers = new double[10];
+for (int i = 0; i < 10; i++)
+{
+    numbers[i] = new Random().NextDouble() * 100;
+}
+
+double min = numbers[0];
+int minIndex = 0;
+for (int j = 0; j < 10; j++)
+{
+	if (min > numbers[j])
+	{
+		min = numbers[j];
+		minIndex = j;
+	}
+}
+
+double max = numbers[0];
+int maxIndex = 0;
+for (int y = 0; y < 10; y++)
+{
+	if (max < numbers[y])
+	{
+		max = numbers[y];
+		maxIndex = y;
+	}
+}
+
+Console.WriteLine($"Массив - [{String.Join(";", numbers)}]");
+Console.WriteLine($"Разница между максимальным и минимальным значением {numbers[maxIndex] - numbers[minIndex]}");
+
