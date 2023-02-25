cded29a4d6df067cde7d930e2616be8f2a15a89c
Максим Глинчук <glinchuk.maxim@yandex.ru>
2023-02-25 14:49:31 +0400

    task34

---
+++ task34/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+Console.WriteLine("Задайте размер массива:");
+int[] array = GetArray(Convert.ToInt32(Console.ReadLine()),100,999);
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        count += 1;
+    }
+}
+Console.WriteLine($"В массиве - [{String.Join(";", array)}] находятся {count} четных чисел");
